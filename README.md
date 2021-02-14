2bb06485c7a181edfa5ab974c5b74de56139352d
Tomasz Grzechowski <keo@becomewater.com>
2021-02-14 19:09:45 +0100

    base

---
+++ README.md
@@ -0,0 +1,3 @@
+# merito-lvssb - Merito Laravel VUE SaaS SPA Bundle
+
+This is a little thing to help me build stuff quicker in 2021. Use it any way you please.
---
+++ src/Grzechowski/merito-lvssb/MeritoLvssbServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Grzechowski\MeritoLvssb;
+
+use Illuminate\Support\ServiceProvider;
+
+class MeritoLvssbServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // $this->registerPublishables();
+    }
+
+    public function register()
+    {
+        // $this->registerCommands();
+    }
+
+    protected function registerPublishables(): void
+    {
+
+    }
+
+    protected function registerCommands(): void
+    {
+
+    }
+}
